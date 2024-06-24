0ea95d4ee17099c4f075410783a61dfd3e057976
Victor Amajuoyi <victorgavon@gmail.com>
2024-06-25 00:51:10 +0100

    ES6 Basics Task - 14. Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,3 +1,13 @@
 export default function iterateThroughObject(reportWithIterator) {
-  return reportWithIterator.join(' | ');
+  const iterator = reportWithIterator[Symbol.iterator]();
+  let result = '';
+
+  for (const employee of iterator) {
+    result += `${employee} | `;
+  }
+
+  // Remove the last ' | ' from the end of the string
+  result = result.slice(0, -3);
+
+  return result;
 }
