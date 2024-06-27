e6094441b74b9a9025b02000aab433b5b0eca850
Victor Amajuoyi <victorgavon@gmail.com>
2024-06-27 15:16:30 +0100

    ES6 Classes Task - 2. A Course, Getters, and Setters

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,43 +1,52 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    this._name = name;
-    this._length = length;
-    this._students = students;
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+    if (!Array.isArray(students)) {
+      throw new TypeError('must be a array');
+    } else {
+      this._students = students;
+    }
   }
 
-  // Getter and setter for name
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
+  set name(na) {
+    if (typeof na !== 'string') {
       throw new TypeError('Name must be a string');
     }
-    this._name = value;
+    this._name = na;
   }
 
-  // Getter and setter for length
   get length() {
     return this._length;
   }
 
-  set length(value) {
-    if (typeof value !== 'number') {
+  set length(zise) {
+    if (typeof zise !== 'number') {
       throw new TypeError('Length must be a number');
     }
-    this._length = value;
+    this._length = zise;
   }
 
-  // Getter and setter for students
   get students() {
     return this._students;
   }
 
-  set students(value) {
-    if (!Array.isArray(value) || !value.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+  set students(stu) {
+    if (!Array.isArray(stu)) {
+      throw new TypeError('must be a array');
     }
-    this._students = value;
+    this._students = stu;
   }
 }
