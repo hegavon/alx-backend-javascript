b929d78cffe5b6162f693848f67de9bce2bd80ea
Victor Amajuoyi <victorgavon@gmail.com>
2024-07-02 14:04:15 +0100

    Data Manipulation - Task 2. Filter

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,4 @@
-function getStudentsByLocation(students, city) {
+// 2-get_students_by_loc.js
+export default function getStudentsByLocation(students, city) {
   return students.filter((student) => student.location === city);
 }
-export default getStudentsByLocation;
