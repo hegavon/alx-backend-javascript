0d1982e5186cb8d4fba4e48ab06a731544694a5c
Victor Amajuoyi <victorgavon@gmail.com>
2024-08-20 23:42:52 +0100

    Node JS - Task 8. Organize a complex HTTP server using Express

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
 class AppController {
-    static getHomepage(request, response) {
-      response.status(200).send('Hello Holberton School!');
-    }
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
   }
-  
-  module.exports = AppController;
+}
+
+module.exports = AppController;
