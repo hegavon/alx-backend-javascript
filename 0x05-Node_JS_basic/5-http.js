dde9f9ecd09ea53fbe6fe12d1f3bae16b6e1ad91
Victor Amajuoyi <victorgavon@gmail.com>
2024-08-20 22:23:12 +0100

    Node JS - Task 5. Create a more complex HTTP server using Node's HTTP module

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,38 +1,73 @@
 #!/usr/bin/env node
 
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const { readFile } = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 1245;
 
-// Create an HTTP server instance
-const app = http.createServer((req, res) => {
-  // Set the response header to plain text
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
+function countStudents(fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
 
-  // Check the URL path and send the appropriate response
-  if (req.url === '/') {
-    res.end('Hello Holberton School!');
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    // Call the countStudents function with the database file path
-    countStudents(process.argv[2])
-      .then(() => {
-        res.end();
-      })
-      .catch((error) => {
-        res.end(error.message);
-      });
-  } else {
-    res.end(' ');
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
+  }
+  if (request.url === '/students') {
+    response.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      response.end(outString);
+    }).catch(() => {
+      response.statusCode = 404;
+      response.end('Cannot load the database');
+    });
   }
 });
 
-// Make the server listen on the specified port and hostname
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-// Export the app variable
 module.exports = app;
