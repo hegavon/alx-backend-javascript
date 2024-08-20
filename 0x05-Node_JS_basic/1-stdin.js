5a3b1460aa004281c77404275184742185069e88
Victor Amajuoyi <victorgavon@gmail.com>
2024-08-20 16:53:43 +0100

    Node JS - Task 1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -5,8 +5,8 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 // Listen for user input
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim(); // Get the input and remove any extra whitespace
-    console.log(`Your name is: ${name}`);
-    process.stdout.write('This important software is now closing\n');
-    process.exit(); // Exit the program after displaying the closing message
+  const name = data.toString().trim(); // Get the input and remove any extra whitespace
+  console.log(`Your name is: ${name}`);
+  process.stdout.write('This important software is now closing\n');
+  process.exit(); // Exit the program after displaying the closing message
 });
